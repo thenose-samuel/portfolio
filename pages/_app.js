998b1adeaf1912c5778ad5a702abc1493dbd5f15
Samuel Khongthaw <skthaw1@gmail.com>
2023-03-28 19:28:20 +0530

    changed colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import "@fontsource/montserrat/800.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/400.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
