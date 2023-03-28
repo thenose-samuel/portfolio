3f8696503f4eca9c5da1ff1582c01b3d4a315b8b
Samuel Khongthaw <skthaw1@gmail.com>
2023-03-28 19:57:14 +0530

    changed font

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import "@fontsource/montserrat/700.css";
 import "@fontsource/montserrat/600.css";
 import "@fontsource/montserrat/500.css";
 import "@fontsource/montserrat/400.css";
+import "@fontsource/mukta/600.css";
+import "@fontsource/mukta/500.css";
+import "@fontsource/mukta/400.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -31,14 +31,14 @@ export default function Home({posts}) {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <div className='flex flex-col bg-black h-screen'>
-      <div id='navbar' className='sticky top-0 z-10 backdrop-filter backdrop-blur-md border-b border-gray-200 bg-opacity-30 flex justify-end font-monserrat font-bold items-center bg-black h-14  pr-2 md:pr-4'>
-        <div className='pr-4 md:pr-4 text-neutral-400 font-montserrat text-sm'>Blogs</div>
+      <div id='navbar' style={{fontFamily: 'Mukta', fontWeight: '600'}} className='tracking-wider sticky top-0 z-10 backdrop-filter backdrop-blur-md border-b border-gray-200 bg-opacity-30 flex justify-end font-bold items-center bg-black h-14  pr-2 md:pr-4'>
+        <div className='pr-4 md:pr-4 text-neutral-400 text-sm'>Blogs</div>
         <div className='text-neutral-400 text-sm'>Gallery</div>
       </div>
       <div className='flex flex-col items-center'>
         <div className='max-w-2xl'>
-          <div className='text-4xl md:text-5xl font-extrabold font-mono pt-20 pb-10 pl-4 w-fit from-purple-700 to-pink-600 cursor-pointer text-transparent bg-clip-text bg-gradient-to-r hover:from-pink-600 hover:to-purple-700 duration-300'>Hey!</div>
-          <div className='text-lg md:text-lg font-montserrat pl-4 container text-neutral-300'>I&#39;m Samuel Khongthaw a CS junior at <span className='font-bold'>NIT, Meghalaya</span>. I enjoy working with Flutter and NextJS. Check out my <Link className='text-blue-600 hover:text-red-900' href={'https://www.youtube.com/channel/UCtZdrKp44G37iG7Hbd_La1A'}>channel</Link> on YouTube 📺. You can also connect with me on <Link href='https://www.linkedin.com/in/samuel-khongthaw-1a03a8206/' className='text-blue-600 hover:text-red-900'>LinkedIn</Link>.</div>
+          <div className='text-4xl md:text-5xl font-bold font-montserrat pt-20 pb-10 pl-4 w-fit from-purple-700 to-pink-600 cursor-pointer text-transparent bg-clip-text bg-gradient-to-r hover:from-pink-600 hover:to-purple-700 duration-300'>Hey!</div>
+          <div style={{fontFamily: 'Mukta', fontWeight: '400'}} className='tracking-wider text-lg md:text-lg pl-4 container text-neutral-300'>I&#39;m Samuel Khongthaw a CS junior at <span className='font-bold'>NIT, Meghalaya</span>. I enjoy working with Flutter and NextJS. Check out my <Link className='text-blue-600 hover:text-red-900' href={'https://www.youtube.com/channel/UCtZdrKp44G37iG7Hbd_La1A'}>channel</Link> on YouTube 📺. You can also connect with me on <Link href='https://www.linkedin.com/in/samuel-khongthaw-1a03a8206/' className='text-blue-600 hover:text-red-900'>LinkedIn</Link>.</div>
           {/* <div className='pl-4 font-mono pt-10 text-neutral-100'>
             Recent Blogs
           </div> */}
