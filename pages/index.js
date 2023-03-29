e98ca98fe09ddf50ca2c47caec8290093a7d1718
Samuel Khongthaw <skthaw1@gmail.com>
2023-03-29 20:33:07 +0530

    expanded info

--- pages/index.js
+++ pages/index.js
@@ -32,24 +32,16 @@ export default function Home({posts}) {
       </Head>
       <div className='flex flex-col bg-black h-screen'>
       <div id='navbar' style={{fontFamily: 'Mukta', fontWeight: '600'}} className='tracking-wider sticky top-0 z-10 backdrop-filter backdrop-blur-md border-b border-gray-200 bg-opacity-30 flex justify-end font-bold items-center bg-black h-14  pr-2 md:pr-4'>
-        <div className='pr-4 md:pr-4 text-neutral-400 text-sm'>Blogs</div>
-        <div className='text-neutral-400 text-sm'>Gallery</div>
+        <div className='pr-4 md:pr-4 text-neutral-400 text-sm cursor-pointer'>Blogs</div>
+        <div className='text-neutral-400 text-sm cursor-pointer'>Gallery</div>
       </div>
       <div className='flex flex-col items-center'>
         <div className='max-w-2xl'>
           <div className='text-4xl md:text-5xl font-bold font-montserrat pt-20 pb-10 pl-4 w-fit from-purple-700 to-pink-600 cursor-pointer text-transparent bg-clip-text bg-gradient-to-r hover:from-pink-600 hover:to-purple-700 duration-300'>Hey!</div>
-          <div style={{fontFamily: 'Mukta', fontWeight: '400'}} className='tracking-wider text-lg md:text-lg pl-4 container text-neutral-300'>I&#39;m Samuel Khongthaw a CS junior at <span className='font-bold'>NIT, Meghalaya</span>. I enjoy working with Flutter and NextJS. Check out my <Link className='text-blue-600 hover:text-red-900' href={'https://www.youtube.com/channel/UCtZdrKp44G37iG7Hbd_La1A'}>channel</Link> on YouTube 📺. You can also connect with me on <Link href='https://www.linkedin.com/in/samuel-khongthaw-1a03a8206/' className='text-blue-600 hover:text-red-900'>LinkedIn</Link>.</div>
-          {/* <div className='pl-4 font-mono pt-10 text-neutral-100'>
-            Recent Blogs
-          </div> */}
-          {/* <div id='blog-section font-mont' className='pt-6 pl-4'>
-            {posts.map((post) => {
-              return <Link href={`/blog/${post.slug}`} key={post.slug}>
-                      <div className="font-bold text-xl">{post.frontMatter.title}</div>
-                      <div className='text-gray-600'>{post.frontMatter.metaDesc}</div>
-                     </Link>
-            })}
-          </div> */}
+          <div style={{fontFamily: 'Mukta', fontWeight: '400'}} className='tracking-wider text-lg md:text-lg pl-4 container text-neutral-300'>I&#39;m Samuel Khongthaw a CS junior at <span className='font-bold'>NIT, Meghalaya</span>. I enjoy working with Flutter and NextJS. Check out my <Link className='text-pink-500 hover:text-red-900 underline' href={'https://www.youtube.com/channel/UCtZdrKp44G37iG7Hbd_La1A'}>channel</Link> on YouTube. You can also connect with me on <Link href='https://www.linkedin.com/in/samuel-khongthaw-1a03a8206/' className='text-pink-500 hover:text-red-900 underline'>LinkedIn</Link>.</div>
+          <div style={{fontFamily: 'Mukta', fontWeight: '400'}} className='tracking-wider text-lg md:text-lg pl-4 container text-neutral-300'>
+            When I'm not making stuffs, you'd probably find me watching a Scorsese movie (yes I love Robert De Niro) or listening to <Link className='text-green-600 hover:text-red-900' href={'https://open.spotify.com/album/6wRev1uYL0JsMsWqktJuVi?autoplay=true'}>Kissland </Link> or <Link href='https://open.spotify.com/album/5EbpxRwbbpCJUepbqVTZ1U?autoplay=true' className='text-neutral-600 hover:text-red-900'>Trilogy</Link>.
+            </div>
         </div>
       </div>
       </div>
